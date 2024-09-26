85337272492d1b784a59a3227721dc2a95e89442
Aditya Gupta <gaditya060@gmail.com>
2024-09-26 23:24:50 +0530

    remove suspense boundary

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,7 +1,6 @@
 import EventCard from "@/components/EventCard";
-import { Suspense } from "react";
 
-async function EventsComponent({ searchParams }) {
+export default async function EventsComponent({ searchParams }) {
     const response = await fetch("https://qevent-backend.labs.crio.do/events");
     let events = await response.json();
     if (searchParams && searchParams['artist'])
@@ -13,12 +12,5 @@ async function EventsComponent({ searchParams }) {
         <div className="flex flex-row gap-x-2 gap-y-2 px-3 py-3 flex-wrap justify-evenly">
             {events.map(event => (<EventCard eventData={event} />))}
         </div>
-    )
-}
-export default function EventsPage() {
-    return (
-        <Suspense fallback={<div className="h-[80vh] flex flex-row items-center justify-center">Loading...</div>}>
-          <EventsComponent />
-        </Suspense>
     )
 }
