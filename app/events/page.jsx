39baaf0ecd44c95643ff61ec38be01593ffec758
Aditya Gupta <gaditya060@gmail.com>
2024-09-26 22:28:23 +0530

    Make events page a server component

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,28 +1,17 @@
-'use client';
 import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 import EventCard from "@/components/EventCard";
 
-async function EventsComponent() {
-    const searchParam = useSearchParams();
+export default async function EventsComponent({ searchParams }) {
     const response = await fetch("https://qevent-backend.labs.crio.do/events");
     let events = await response.json();
-    if (searchParam?.get('artist'))
-        events = events.filter(event => event.artist === searchParam.get('artist'))
-    if (searchParam?.get('tag'))
-        events = events.filter(event => event.tags.includes(searchParam.get('tag')));
+    if (searchParams['artist'])
+        events = events.filter(event => event.artist === searchParams['artist'])
+    if (searchParams['tag'])
+        events = events.filter(event => event.tags.includes(searchParams['tag']));
 
     return (
         <div className="flex flex-row gap-x-2 gap-y-2 px-3 py-3 flex-wrap justify-evenly">
             {events.map(event => (<EventCard eventData={event} />))}
         </div>
     )
-}
-
-export default function EventsPage() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-          <EventsComponent />
-        </Suspense>
-      );
 }
